46ca664f5e98df555b348dbcf6dc30ab300f4d8b
Jerry Jiahao Xu <jerryhuimanutd@gmail.com>
2019-02-10 14:32:24 -0500

    combine 2 features and feed into RF

--- src/engine1.py
+++ src/engine1.py
@@ -8,8 +8,27 @@ from pyspark.ml.linalg import Vectors
 from pyspark.sql.types import DoubleType
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.feature import IndexToString, StringIndexer, VectorIndexer
+import xgboost as xgb
+
 
 DEBUG = True
+NUM_FEA = 3
+
+if DEBUG:
+    FEA1_TRAIN_PATH = '/Users/jerryhui/Desktop/Files/UGA/CS/8360DataPracticum/Projects/p1/team-void-p1/features/segment_train' 
+    FEA1_TEST_PATH = '/Users/jerryhui/Desktop/Files/UGA/CS/8360DataPracticum/Projects/p1/team-void-p1/features/segment_test'
+    FEA2_TRAIN_PATH = '/Users/jerryhui/Desktop/Files/UGA/CS/8360DataPracticum/Projects/p1/team-void-p1/features/byte_2gram_train' 
+    FEA2_TEST_PATH = '/Users/jerryhui/Desktop/Files/UGA/CS/8360DataPracticum/Projects/p1/team-void-p1/features/byte_2gram_test'
+    FEA3_TRAIN_PATH = '/Users/jerryhui/Desktop/Files/UGA/CS/8360DataPracticum/Projects/p1/team-void-p1/features/byte_3gram_train' 
+    FEA3_TEST_PATH = '/Users/jerryhui/Desktop/Files/UGA/CS/8360DataPracticum/Projects/p1/team-void-p1/features/byte_3gram_test'
+else:
+    FEA1_TRAIN_PATH = 'gs://uga-8360-projects/features/segment_train' 
+    FEA1_TEST_PATH = 'gs://uga-8360-projects/features/segment_test'
+    FEA2_TRAIN_PATH = 'gs://uga-8360-projects/features/byte_2gram_train' 
+    FEA2_TEST_PATH = 'gs://uga-8360-projects/features/byte_2gram_test'
+    FEA3_TRAIN_PATH = 'gs://uga-8360-projects/features/byte_3gram_train' 
+    FEA3_TEST_PATH = 'gs://uga-8360-projects/features/byte_3gram_test'
+
 
 def _format_(rdd, feature_path):
     f = feature_path.lower()
@@ -18,31 +37,82 @@ def _format_(rdd, feature_path):
             rdd_fea = rdd.map(lambda x: ((x[0], x[1], int(x[2])), x[3]))
         elif 'gram' in f: 
             rdd_fea = rdd.map(lambda x: ((x[0], x[1], x[3]), Vectors.dense(list(x[2].toArray()))))
-        elif 'file' in f: 
-            rdd_fea = rdd.map(lambda x: ((x[0], x[1], x[5]), Vectors.dense(list((x[2], x[3], x[4])))))
     if 'test' in f:
         if 'segment' in f: 
             rdd_fea = rdd.map(lambda x: ((x[1], x[0]), x[2]))
         elif 'gram' in f: 
             rdd_fea = rdd.map(lambda x: ((x[0], x[1]), Vectors.dense(list(x[2].toArray()))))
-        elif 'file' in f: 
-            rdd_fea = rdd.map(lambda x: ((x[0], x[1]), Vectors.dense(list((x[2], x[3], x[4])))))
     return rdd_fea
 
 
-def RF_format(df):
-    # Input >> DF(docid, hash, label, vector.dense(features))
-    stringIndexer = StringIndexer(inputCol = "hash", outputCol = "indexed")
-    si_model = stringIndexer.fit(df)
-    td = si_model.transform(df)
-    return td
+def get_dataframe(NUM_FEA):
+    if NUM_FEA == 1:
+        ## features: (segments)
+        # ------------------------------------------------------------------------
+        rdd_train = spark.read.parquet(FEA1_TRAIN_PATH).rdd.map(tuple)
+        rdd_test = spark.read.parquet(FEA1_TEST_PATH).rdd.map(tuple)
 
+        if 'segment' in FEA1_TRAIN_PATH.lower():
+            rdd_test = rdd_test.map(lambda x: (x[1], x[0], x[2]))
+        if 'gram' in FEA1_TRAIN_PATH.lower():
+            rdd_train = rdd_train.map(lambda x: (x[0], x[1], x[3], Vectors.dense(list(x[2].toArray()))))
+            rdd_test = rdd_test.map(lambda x: (x[0], x[1], Vectors.dense(list(x[2].toArray()))))
 
-def RF_model(td, n, m):
-    td_new = td.withColumn("label", td["label"].cast(DoubleType()))
-    rf = RandomForestClassifier(numTrees=n, maxDepth=m)
-    model = rf.fit(td_new)
-    return model
+    elif NUM_FEA == 2:
+        ## features, (segments, bytes_2gram)
+        # ------------------------------------------------------------------------
+        fea1_train_rdd = spark.read.parquet(FEA1_TRAIN_PATH).rdd.map(tuple)
+        fea1_train = _format_(fea1_train_rdd, FEA1_TRAIN_PATH.split('/')[-1])
+        fea2_train_rdd = spark.read.parquet(FEA2_TRAIN_PATH).rdd.map(tuple)
+        fea2_train = _format_(fea2_train_rdd, FEA2_TRAIN_PATH.split('/')[-1])
+        
+        rdd_train = fea1_train.leftOuterJoin(fea2_train).map(lambda x: (x[0][0], x[0][1], x[0][2], Vectors.dense(list(x[1][0]) + list(x[1][1]))))
+
+        fea1_test_rdd = spark.read.parquet(FEA1_TEST_PATH).rdd.map(tuple)
+        fea1_test = _format_(fea1_test_rdd, FEA1_TEST_PATH.split('/')[-1])
+        fea2_test_rdd = spark.read.parquet(FEA2_TEST_PATH).rdd.map(tuple)
+        fea2_test = _format_(fea2_test_rdd, FEA2_TEST_PATH.split('/')[-1])
+        
+        rdd_test = fea1_test.leftOuterJoin(fea2_test).map(lambda x: (x[0][0], x[0][1], Vectors.dense(list(x[1][0]) + list(x[1][1]))))
+
+    elif NUM_FEA == 3:
+        ## features, (segments, bytes_2gram, bytes_3gram)
+        # ------------------------------------------------------------------------
+        fea1_train_rdd = spark.read.parquet(FEA1_TRAIN_PATH).rdd.map(tuple)
+        fea1_train = _format_(fea1_train_rdd, FEA1_TRAIN_PATH)
+        fea2_train_rdd = spark.read.parquet(FEA2_TRAIN_PATH).rdd.map(tuple)
+        fea2_train = _format_(fea2_train_rdd, FEA2_TRAIN_PATH)
+        fea3_train_rdd = spark.read.parquet(FEA3_TRAIN_PATH).rdd.map(tuple)
+        fea3_train = _format_(fea3_train_rdd, FEA3_TRAIN_PATH)
+        rdd_train = fea1_train.leftOuterJoin(fea2_train)\
+                        .map(lambda x: (x[0], Vectors.dense(list(x[1][0]) + list(x[1][1]))))\
+                        .leftOuterJoin(fea3_train)\
+                        .map(lambda x: (x[0][0], x[0][1], x[0][2], Vectors.dense(list(x[1][0]) + list(x[1][1]))))
+
+        fea1_test_rdd = spark.read.parquet(FEA1_TEST_PATH).rdd.map(tuple)
+        fea1_test = _format_(fea1_test_rdd, FEA1_TEST_PATH)
+        fea2_test_rdd = spark.read.parquet(FEA2_TEST_PATH).rdd.map(tuple)
+        fea2_test = _format_(fea2_test_rdd, FEA2_TEST_PATH)
+        fea3_test_rdd = spark.read.parquet(FEA3_TEST_PATH).rdd.map(tuple)
+        fea3_test = _format_(fea3_test_rdd, FEA3_TEST_PATH)
+        rdd_test = fea1_test.leftOuterJoin(fea2_test)\
+                        .map(lambda x: (x[0], Vectors.dense(list(x[1][0]) + list(x[1][1]))))\
+                        .leftOuterJoin(fea3_test)\
+                        .map(lambda x: (x[0][0], x[0][1], Vectors.dense(list(x[1][0]) + list(x[1][1]))))
+    else:
+        exit()
+
+    print('***********************************************')
+    print('Training Dataframe')
+    df_train = spark.createDataFrame(rdd_train, ['docid', 'hash', 'label', 'features'])
+    # print(df_train.show(n=2, truncate=140))
+    
+    print('***********************************************')
+    print('Testing Dataframe')
+    df_test = spark.createDataFrame(rdd_test, ['docid', 'hash', 'features'])
+    # print(df_test.show(n=2, truncate=140))
+
+    return df_train, df_test
 
 
 def cal_accuracy(label_list, pred_list):
@@ -67,62 +137,26 @@ if __name__ == "__main__":
     spark = SparkSession.builder.master("local").appName("Word Count").config("spark.some.config.option", "some-value").getOrCreate()
     
     num_trees = 50
-    max_depth = 10
+    max_depth = 25
 
-    if DEBUG:
-        fea1_train_path = '/Users/jerryhui/Desktop/Files/UGA/CS/8360DataPracticum/Projects/p1/team-void-p1/features/segment_train_jx' 
-        fea1_test_path = '/Users/jerryhui/Desktop/Files/UGA/CS/8360DataPracticum/Projects/p1/team-void-p1/features/segment_test_jx'
-        fea2_train_path = '/Users/jerryhui/Desktop/Files/UGA/CS/8360DataPracticum/Projects/p1/team-void-p1/features/opcode_1gram_train' 
-        fea2_test_path = '/Users/jerryhui/Desktop/Files/UGA/CS/8360DataPracticum/Projects/p1/team-void-p1/features/opcode_1gram_test'
-    else:
-        fea1_train_path = 'gs://uga-8360-projects/features/segment_train_jx' 
-        fea1_test_path = 'gs://uga-8360-projects/features/segment_test_jx'
-        fea2_train_path = 'gs://uga-8360-projects/features/opcode_1gram_train' 
-        fea2_test_path = 'gs://uga-8360-projects/features/opcode_1gram_test'
-
-    ## features, (segment, opcode-bigram)
-    # ------------------------------------------------------------------------
-    fea1_train_rdd = spark.read.parquet(fea1_train_path).rdd.map(tuple)
-    fea1_train = _format_(fea1_train_rdd, fea1_train_path.split('/')[-1])
-    fea2_train_rdd = spark.read.parquet(fea2_train_path).rdd.map(tuple)
-    fea2_train = _format_(fea2_train_rdd, fea2_train_path.split('/')[-1])
-    
-    rdd_train = fea1_train.leftOuterJoin(fea2_train).map(lambda x: (x[0][0], x[0][1], x[0][2], Vectors.dense(list(x[1][0]) + list(x[1][1]))))
-
-    fea1_test_rdd = spark.read.parquet(fea1_test_path).rdd.map(tuple)
-    fea1_test = _format_(fea1_test_rdd, fea1_test_path.split('/')[-1])
-    fea2_test_rdd = spark.read.parquet(fea2_test_path).rdd.map(tuple)
-    fea2_test = _format_(fea2_test_rdd, fea2_test_path.split('/')[-1])
-    
-    rdd_test = fea1_test.leftOuterJoin(fea2_test).map(lambda x: (x[0][0], x[0][1], Vectors.dense(list(x[1][0]) + list(x[1][1]))))
-
-    print('***********************************************')
-    print('Training Dataframe')
-    df_train = spark.createDataFrame(rdd_train, ['docid', 'hash', 'label', 'features'])
-    # print(df_train.show(n=2, truncate=140))
-    
-    print('***********************************************')
-    print('Testing Dataframe')
-    df_test = spark.createDataFrame(rdd_test, ['docid', 'hash', 'features'])
-    # print(df_test.show(n=2, truncate=140))
-
-    # ------------------------------------------------------------------------
-
+    df_train, df_test = get_dataframe(NUM_FEA)
 
     # Random Forest Classification
     ##########################################################################
-    model = RF_model(df_train, n=num_trees, m=max_depth)
-    # pred = model.transform(RF_format(df_test))
+    rf = RandomForestClassifier(numTrees=num_trees, maxDepth=max_depth)
+    model = rf.fit(df_train.withColumn("label", df_train["label"].cast(DoubleType())))
+
     pred = model.transform(df_test)
     pred = pred.withColumn("prediction", pred["prediction"].cast("int"))
-    print(pred.show(n=10))
+
     y_test = pred.select("docid", "prediction").rdd.map(tuple).sortByKey().map(lambda x: x[1]).collect()
 
+
     # Accuracy
-    # rdd_ytest = sc.textFile('files/y_small_test.txt')
+    # rdd_ytest = sc.textFile('gs://uga-dsp/project1/files/y_small_test.txt')
     # accuracy = cal_accuracy(rdd_ytest.collect(), y_test)
     # print('Testing Accuracy: %.2f %%' % (accuracy*100))
     # print('**********************************************')
 
     # Output file
-    output_file(y_test, 'prediction1.txt')
+    output_file(y_test, 'prediction.txt')
