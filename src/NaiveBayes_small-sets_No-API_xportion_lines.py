46b567500345a369f7c482330fb5bdc08fc7da0c
mrezaim <mohammadreza.iman@hotmail.com>
2019-02-10 15:23:47 -0500

    The two no_API used codes got optimized on reading and mapping also on classifying documents. Another code added, which use simpler feature, only a begining portion of each lines, and performed well. The new code name is NaiveBayes_small-sets_No-API_xportion_lines.

--- src/NaiveBayes_small-sets_No-API_xportion_lines.py
+++ src/NaiveBayes_small-sets_No-API_xportion_lines.py
@@ -1,13 +1,19 @@
-## 56% accuracy on small test set
+### Sampling the train set and only use 2.5 grams of begining of each line on bytes for training and predicting
+### 79% accuracy on small test set
 
 ## For running on google cloud dataproc:
 ## Initialization actions: gs://uga-dsp/scripts/conda-dataproc-bootstrap.sh
 ## python -m pip install --user google-cloud-storage
-## pyspark --master yarnexit
+## pyspark --master yarn
+## spark.conf.set("spark.executor.memory", "18g")
 
-import math #Needed for calculating Log
-import pyspark
-sc = pyspark.SparkContext()
+from pyspark import SparkContext
+#need to be configured based on memory of workers:
+spark.conf.set("spark.executor.memory", "18g")
+sc = SparkContext()
+sqlContext = SQLContext(sc)
+
+
 x_small_train = sc.textFile("gs://uga-dsp/project1/files/X_small_train.txt").collect()
 x_small_test = sc.textFile("gs://uga-dsp/project1/files/X_small_test.txt").collect()
 Y_small_train = sc.textFile("gs://uga-dsp/project1/files/y_small_train.txt").collect()
@@ -15,13 +21,14 @@ Y_small_test = sc.textFile("gs://uga-dsp/project1/files/y_small_test.txt").colle
 print("!!!Importing the small datasets is done!!!")
 
 n_fs = 200 #len(x_small_train)
+print("Number of train file: ",n_fs)
 n_tfs = len(x_small_test)
+print("Number of test file: ",n_tfs)
 classes = len(set(Y_small_train)) #distinct number of values in Y train file
-norm = 1 #00
-cutoftf = 20.0
-
+print("Number of classes: ",classes)
 #The function of adding elements of two same size list of integers, which is used in reduce
 def add_a(a,b):
+   print (a,b)
    if len(a)==len(b):
       for ii in range(0,len(a)):
          a[ii]=int(a[ii])+int(b[ii])
@@ -29,47 +36,39 @@ def add_a(a,b):
 
 #Initializing an RDD to union result of each term frequncy calculation into specific index of the value list
 a = [1]*(classes)
-wc3 = sc.parallelize([])
+from pyspark.sql.types import *
+field = [StructField('term',StringType(), True),StructField('tf_classes', ArrayType(DoubleType(), True),True)]
+schema = StructType(field)
 
-#Running term frequncy calculation while cleaning the address in each line and removing terms including "?"; then creat a RDD of all terms with list of TF for each class
-for i in range(0,n_fs):
-   f = sc.textFile("gs://uga-dsp/project1/data/bytes/"+x_small_train[i]+".bytes")
-   arr = [0]*(classes)
-   arr[int(Y_small_train[i])-1]=1
-   wc= f.flatMap(lambda x: x.split('\n')).map(lambda x: x[9:]).map(lambda x: (x,arr)).combineByKey(list,add_a,add_a)
-   wc2 = wc.filter(lambda x: "?" not in x[0])
-   wc3 = wc2.union(wc3).combineByKey(list,add_a,add_a)
 
-   
-#The function to calculate TF-IDF for each term for each document using the list of term frequncy calculated above. It replaces the TF for each doc in the list with TF-IDF of such term for the doc
-def cal_tf_idf(arr):
-   dn = 0
+#The function to calculate TF for each term for each class using the list of term frequncy calculated above. 
+def cal_tf_per_cf(arr):
+   sum = 0
    for a in arr:
-      if a > 0: dn +=1
+      sum += a
    for k in range (0,len(arr)):
-      arr[k]=arr[k]*math.log(len(arr)/dn)
+      arr[k]=arr[k]/sum
    return arr  
 
-#Calling the TF-IDF function in RDD format
-wc4 = wc3.map(lambda x: (x[0],cal_tf_idf(x[1])))
+#Running term frequncy calculation while cleaning the address in each line and removing terms including "?"; then creat a RDD of all terms with list of TF for each class
+
+wc3 = sc.parallelize([])
+for i in range(0,n_fs):
+	k += 1
+	f = sc.textFile("gs://uga-dsp/project1/data/bytes/"+x_small_train[i]+".bytes")
+	arr = [0]*(classes)
+	arr[int(Y_small_train[i])-1]=1
+	wc= f.map(lambda x: x[9:16]).filter(lambda x: "?" not in x).map(lambda x: (x,arr))
+	wc3 = wc.union(wc3)
 
 
-#Reduce the size of bag of words by excluding the terms that doesn't have any tf-idf bigger than the cut of value set at begining, so the prediction could work faster
-def cut_of_tf(arr):
-	for a in arr:
-		if a > cutoftf:
-			return True
-	return False
+wc4 = wc3.combineByKey(list,add_a,add_a).map(lambda x: (x[0],cal_tf_per_cf(x[1])))
+wc4.cache()
 
-wc5 = wc4.filter(lambda x: cut_of_tf(x[1]))
-
-wc5.cache()
-
-
-#The function for calculating and accumulating the probabablity of each terms in test doc using the created bag of words from traning documents and applying the normalization value
+#The function for calculating and accumulating the probabablity of each terms in test doc using the created bag of words from traning documents
 def cal_prob(arr):
 	for i in range(0,len(arr[1])):
-		arr[1][i]=1+arr[1][i]*arr[0] #*norm
+		arr[1][i]=arr[1][i]*arr[0]
 	return arr[1]
 
 #Reading test document and calculate NB probabablity using above function and the created bag of words from traning documents
@@ -77,11 +76,13 @@ Pr = [0]*n_tfs
 from operator import add
 for j in range(0,n_tfs):
 	ttf = sc.textFile("gs://uga-dsp/project1/data/bytes/"+x_small_test[j]+".bytes")
-	ttwc= ttf.flatMap(lambda x: x.split('\n')).map(lambda x: x[9:]).filter(lambda x: "?" not in x[0]).map(lambda x: (x,1)).reduceByKey(add)
+	ttwc= ttf.flatMap(lambda x: x.split('\n')).map(lambda x: x[9:16]).filter(lambda x: "?" not in x[0]).map(lambda x: (x,1)).reduceByKey(add)
 	prob = sc.parallelize([])
 	prob = ttwc.leftOuterJoin(wc4).filter(lambda x: x[1][1]!= None).map(lambda x: (j,cal_prob(x[1]))).reduceByKey(add_a)
 	pr2 = prob.collect()
 	Pr[pr2[0][0]] = pr2[0][1] 
+
+
 
 Pre = [0]*n_tfs
 for i in range(0,n_tfs):
@@ -101,11 +102,16 @@ for i in range(0,n_tfs):
 acc = (k/i)
 print (acc)
 
+sc.stop() 
+
 with open("/tt.txt", "w") as output:
-    output.write(str(PredictY))
-
-
+    output.write(str(Pre))
+	
 with open("/acc.txt", "w") as output:
     output.write(str(acc))
 
+
 sc.stop() 
+
+
+
